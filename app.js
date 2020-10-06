f96baf44a7b6576c2bc957880755da143e71da13
Ignacio Merello Lloret <ignaciomerellolloret@gmail.com>
2020-10-06 09:41:35 +0200

    Uptdate app.js

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const app = express();
 
 app.use(express.json());
 
+app.use(express.json()); //middleware: parsea el body JSON y evita que req.body sea undefined
 
 app.listen (PORT, () => console.log('Server running' + PORT));
