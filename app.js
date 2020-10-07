d56bfb4730cdbe92f670417f16c414acbb0e6a96
Ignacio Merello Lloret <ignaciomerellolloret@gmail.com>
2020-10-07 19:45:06 +0200

    Add Router For Movies to Main

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const movieRoutes = require('./routes/movieRoutes');
+
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.json()); //middleware: parsea el body JSON y evita que req.body sea undefined
-
+app.use("/movie", movieRoutes);
 app.listen (PORT, () => console.log('Server running' + PORT));
