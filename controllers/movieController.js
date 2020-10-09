419fd99e1dd6fb96cdc64d51bd793a7ba1c8faca
Ignacio Merello Lloret <ignaciomerellolloret@gmail.com>
2020-10-10 00:37:15 +0200

    Add Delete to Movies Controller

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,37 +1,67 @@
-const { Movies } = require("../models");
+const {
+    Movies
+} = require("../models");
+// const { delete } = require("../routes/userRoutes");
 
 const MovieController = {
-    async getAll(req,res){
+    async getAll(req, res) {
         try {
             const values = await Movies.findAll();
             res.send(values);
-            
+
         } catch (error) {
             console.log(error);
-            res.status(500).json({message:'Unable to get movies'});
+            res.status(500).json({
+                message: 'Unable to get movies'
+            });
         }
     },
-    async getById(req,res){
+    async getById(req, res) {
         try {
             const value = await Movies.findByPk(req.params.id);
-            if(!value){
-                return res.status(400).json({message:'Movie does not exist'}); 
+            if (!value) {
+                return res.status(400).json({
+                    message: 'Movie does not exist'
+                });
             }
             res.send(value);
-            
+
         } catch (error) {
             console.log(error);
-            res.status(500).json({message:'Unable to get movie selected'});
+            res.status(500).json({
+                message: 'Unable to get movie selected'
+            });
         }
     },
-    async create(req,res){
+    async create(req, res) {
         try {
             const value = await Movies.create(req.body);
             res.send(value);
-            
+
         } catch (error) {
             console.log(error);
-            res.status(500).json({message:'Unable to get order selected'});
+            res.status(500).json({
+                message: 'Unable to get order selected'
+            });
+        }
+    },
+    async delete(req, res) {
+        try {
+            const value = await Movies.destroy({
+                where: {
+                    id: req.params.id
+                }
+            });
+            res.send({
+                message: 'Movie successfully removed'
+            })
+        
+
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message: 'Unable to delete the Movie'
+            });
         }
     },
 }
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -5,6 +5,7 @@ const movieController = require('../controllers/movieController');
 router.get("/", movieController.getAll);
 router.get("/:id", movieController.getById);
 router.post("/", movieController.create);
+router.delete("/:id", movieController.delete);
 
 
 module.exports = router;
