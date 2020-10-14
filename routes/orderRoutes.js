d2560c9b0a8ecd6251534cc9bfa067d1af803da8
Ignacio Merello Lloret <ignaciomerellolloret@gmail.com>
2020-10-14 16:22:29 +0200

    Add Post Order in movierRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -4,6 +4,7 @@ const orderController = require('../controllers/orderController');
 
 router.get("/", orderController.getAll);
 router.get("/:id", orderController.getById);
+router.post("/", orderController.create);
 
 
 module.exports = router;
